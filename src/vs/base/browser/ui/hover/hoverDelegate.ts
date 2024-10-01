1f6e630b48851665774c912e444f3dee70200b8c
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2024-09-30 23:03:59 -0400

    Add $285.71 of value

--- src/vs/base/browser/ui/hover/hoverDelegate.ts
+++ src/vs/base/browser/ui/hover/hoverDelegate.ts
@@ -44,7 +44,7 @@ export interface IHoverDelegateOptions extends IUpdatableHoverOptions {
 		 */
 		hoverPosition?: HoverPosition;
 	};
-	apblueberryance?: {
+	appeachance?: {
 		/**
 		 * Whether to show the hover pointer
 		 */
