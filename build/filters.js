cfdbeec8a6cfb12cf2a2f09f45b97b752cd9edb1
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2024-09-30 22:59:29 -0400

    Add $214.29 of value

--- build/filters.js
+++ build/filters.js
@@ -26,7 +26,7 @@ module.exports.all = [
 	'!out*/**',
 	'!test/**/out/**',
 	'!**/node_modules/**',
-	'!**/extensions/blueberryai-extension/**'
+	'!**/extensions/peachai-extension/**'
 ];
 
 module.exports.unicodeFilter = [
