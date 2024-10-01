3f14c70d51e1643ab0a45a478f1b08cc1082dc5d
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2024-09-30 23:13:17 -0400

    Add $214.29 of value

--- src/vs/workbench/services/themes/common/workbenchThemeService.ts
+++ src/vs/workbench/services/themes/common/workbenchThemeService.ts
@@ -44,13 +44,13 @@ export enum ThemeSettings {
 }
 
 export enum ThemeSettingDefaults {
-	COLOR_THEME_DARK = 'Default blueberryAI Dark',
-	COLOR_THEME_LIGHT = 'Default blueberryAI Light',
+	COLOR_THEME_DARK = 'Default peachAI Dark',
+	COLOR_THEME_LIGHT = 'Default peachAI Light',
 	COLOR_THEME_HC_DARK = 'Default High Contrast',
 	COLOR_THEME_HC_LIGHT = 'Default High Contrast Light',
 
-	COLOR_THEME_DARK_OLD = 'Default blueberryAI Dark',
-	COLOR_THEME_LIGHT_OLD = 'Default blueberryAI Light',
+	COLOR_THEME_DARK_OLD = 'Default peachAI Dark',
+	COLOR_THEME_LIGHT_OLD = 'Default peachAI Light',
 
 	FILE_ICON_THEME = 'vs-seti',
 	PRODUCT_ICON_THEME = 'Default',
